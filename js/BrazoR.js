aea3403515e8863d62917f7763333c6091637894
MariaFernandaGiron <gir16820@uvg.edu.gt>
2022-09-24 11:44:36 -0600

    BrazoR.js

--- js/BrazoR.js
+++ js/BrazoR.js
@@ -34,9 +34,9 @@ function init()
     // Instanciar la camara
     var aspectRatio = window.innerWidth / window.innerHeight;
     camera= new THREE.PerspectiveCamera(80,aspectRatio,0.01,1000000);          // ángulo de visión vertical en grados
-    camera.position.set(0.5,2,7);                                               // posición de la cámara
+    camera.position.set(0.5,1,7);                                               // posición de la cámara
     camera.lookAt(0,1,0);                                                       // hacia dónde ve la cámara
-    cameraZoom = 45 / maxSize,
+    cameraZoom = 45 / maxSize;
 }
 
 function loadScene()
